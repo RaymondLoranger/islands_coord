d7c713c5954a7ac7c90358f814a1e5156c2825c8
RaymondLoranger <Raymond.Loranger@bell.net>
2019-03-24 11:06:31 -0400

    0.1.0

--- test/islands/coord_test.exs
+++ test/islands/coord_test.exs
@@ -5,6 +5,18 @@ defmodule Islands.CoordTest do
 
   doctest Coord
 
+  describe "A coord struct" do
+    test "can be encoded by Poison" do
+      {:ok, coord} = Coord.new(1, 10)
+      assert Poison.encode!(coord) == ~s<{\"row\":1,\"col\":10}>
+    end
+
+    test "can be encoded by Jason" do
+      {:ok, coord} = Coord.new(1, 10)
+      assert Jason.encode!(coord) == ~s<{\"col\":10,\"row\":1}>
+    end
+  end
+
   describe "Coord.new/2" do
     test "returns {:ok, ...} given valid args" do
       assert Coord.new(1, 10) == {:ok, %Coord{row: 1, col: 10}}
@@ -14,16 +26,6 @@ defmodule Islands.CoordTest do
       assert Coord.new(0, 10) == {:error, :invalid_coordinate}
       assert Coord.new(-1, 2) == {:error, :invalid_coordinate}
       assert Coord.new("1", "2") == {:error, :invalid_coordinate}
-    end
-
-    test "can be encoded by Poison" do
-      {:ok, coord} = Coord.new(1, 10)
-      assert Poison.encode!(coord) == ~s<{\"row\":1,\"col\":10}>
-    end
-
-    test "can be encoded by Jason" do
-      {:ok, coord} = Coord.new(1, 10)
-      assert Jason.encode!(coord) == ~s<{\"col\":10,\"row\":1}>
     end
   end
 end
