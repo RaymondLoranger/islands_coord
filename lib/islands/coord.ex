566444fd45ff1129c977a9724e05ed9069b03ed7
RaymondLoranger <Raymond.Loranger@bell.net>
2023-02-10 20:51:44 -0500

    0.1.28

--- lib/islands/coord.ex
+++ lib/islands/coord.ex
@@ -8,7 +8,7 @@ defmodule Islands.Coord do
   The coordinates struct contains the fields `row` and `col` representing the
   coordinates of a square in the _Game of Islands_.
 
-  ##### Based on the book [Functional Web Development](https://pragprog.com/book/lhelph/functional-web-development-with-elixir-otp-and-phoenix) by Lance Halvorsen.
+  ##### Based on the book [Functional Web Development](https://pragprog.com/titles/lhelph/functional-web-development-with-elixir-otp-and-phoenix/) by Lance Halvorsen.
   """
 
   alias __MODULE__
